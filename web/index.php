1936f86119c0a04883fd3aec7525474ae18e6419
Drew Quinn <apquinn@gmail.com>
2022-11-30 11:26:09 -0500

    tracking space on right

--- web/index.php
+++ web/index.php
@@ -16,6 +16,174 @@
       </div>
     </div>
 
+    <div class="special-global-wrapper">
+      <div class="special-wrapper">
+        <div class="special-section1">
+          <h1>What makes me so special?</h1>
+          <p>
+            I'm a graduate of Michigan State University with simultaneous
+            degrees in computer science, industrial psychology and minor in art
+            history. I began my career at Accenture, followed by creating dozens
+            of web-based applications and then helping to start what was for a
+            short time a billion-dollar startup. After living the super-fast
+            paced life of a consultant and software architect through the
+            booming of the Internet, I now live a quieter, but nonetheless
+            technically challenging, life running my software business, Sea
+            Quinn, from an 80 acres forest in Michigan's Upper Peninsula. Life
+            on the shores of Lake Superior is unmatched with vast beaches in the
+            summer and winters with over 200 inches of snow. The life of skiing
+            and snowshoeing in the winter and hiking and swimming in the summer
+            are shared with my wife, Amy, and our two dogs, Wondermutt the
+            Australian cattle dog and Maisy the Rottweiler.
+          </p>
+          <h4>Why Sea Quinn?</h4>
+          <p>
+            Sea Quinn was the name of my dad's boat. So many great memories that
+            are now memorialized with my company name.
+          </p>
+        </div>
+
+        <div class="special-section2">
+          <img
+            alt="Drew Quinn"
+            class="responsive-img"
+            src="./bioIncludes/pics/me.jpg"
+          />
+          <p>
+            "The unchanged passion of my life has been to take ideas and make
+            them reality. Whether they are technical innovations or things made
+            of wood, I've got the expertise to make it happen."
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="blue">
+      <div class="special-global-wrapper">
+        <div class="blueBackground">
+          <div class="blue-wrapper bar">
+            <div>
+              <img alt="What do I do" src="./bioIncludes/pics/what.jpg" />
+            </div>
+            <h4>What do I do?</h4>
+            <p>
+              I'm a software architect. From big to small, large teams to solo
+              project.
+            </p>
+            <p><a href="#projects">Check out my experience</a></p>
+          </div>
+          <div class="blue-wrapper bar">
+            <div>
+              <img alt="My expertise" src="./bioIncludes/pics/my.jpg" />
+            </div>
+            <h4>My expertise</h4>
+            <p>
+              Full-lifecycle web development projects and all the technologies
+              required to deliver them.
+            </p>
+            <p><a href="#skills">See my skills</a></p>
+          </div>
+          <div class="blue-wrapper">
+            <div>
+              <img alt="How can I help" src="./bioIncludes/pics/how.jpg" />
+            </div>
+            <h4>How can I help?</h4>
+            <p>
+              I have 15 years working in the LAMP stack. If either works for you, I'm your guy.
+            </p>
+            <p><a href="#help">Get in touch</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="blackBackground">
+      <div class="container">
+        <div class="carousel">
+          <div class="slider">
+            <section>
+              <span class="people-header">
+                <p>What people are saying</p>
+              </span>
+
+              <span class="people-title"
+                >Regarding 2019-20 Drupal 7 to 8 migration</span
+              >
+
+              <span class="people-text">
+                <p>
+                  "Every few years we attempt the impossible. We take our fully
+                  functioning, yet somewhat dated website, and we migrate the
+                  content into a new version of the site. This year we didn't
+                  just start migrating content, we also started the process that
+                  will rewrite the underlying framework that powers the NMU
+                  website. As with past conversions, Drew has played a critical
+                  role in this process by not only figuring out how we can
+                  accomplish this migration but also automating large pieces of
+                  it making the migration of over 40 thousand nodes actually
+                  possible."
+                </p>
+              </span>
+
+              <span class="people-who">
+                <p>
+                  <strong>Eric Johnson</strong><br />
+                  Web Systems Director<br />
+                  Northern Michigan University
+                </p>
+              </span>
+            </section>
+
+            <section>
+              <span class="people-header">
+                <p>What people are saying</p>
+              </span>
+              <span class="people-title"
+                >Regarding Transition During 2020 Epidemic</span
+              >
+              <span class="people-text">
+                <p>
+                  "Working remotely and staying remote for a good part of the
+                  previous year was challenging for everyone on the team. Our
+                  normal office environment and processes were upended by an
+                  unprecedented pandemic. Regardless of this, Drew has embraced
+                  working remotely and even thrived in this new environment.
+                  Through his use of technology and willingness to initiate
+                  communication with the team, Drew has become one of the
+                  primary reasons why we can continue working at a high level
+                  regardless of where our individual offices are located. Beyond
+                  being able to take the lead on a highly technical project,
+                  which Drew has certainly done, being able to effectively
+                  communicate with everyone on the team and leverage their
+                  strengths to advance a project continues to be one of Drew's
+                  invaluable assets."
+                </p>
+              </span>
+              <span class="people-who">
+                <p>
+                  <strong>Eric Johnson</strong><br />
+                  Web Systems Director<br />
+                  Northern Michigan University
+                </p>
+              </span>
+            </section>
+          </div>
+          <div class="controls">
+            <span class="arrow left"
+              ><img src="bioIncludes/pics/chevron-left.png"
+            /></span>
+            <span class="arrow right"
+              ><img src="bioIncludes/pics/chevron-right.png"
+            /></span>
+            <ul>
+              <li class="selected"></li>
+              <li></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+
 	</body>
 </html>
 
