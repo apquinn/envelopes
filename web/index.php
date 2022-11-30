d3532dec149f05ab9bd0a91fdfcd357990bf0dbd
Drew Quinn <apquinn@gmail.com>
2022-11-30 11:10:29 -0500

    added index.php

---
+++ web/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <link rel="stylesheet" href="./bioIncludes/App.css?<?php echo time(); ?>" />
+		<meta name="description" content="Sea-Quinn is a software development and systems integration consoluting firm with full stack development services owned by Drew Quinn.">
+		<meta name="keywords" content="marquette developer, u.p. developer, marquette software, software development, information technology" />
+		<meta name="google-site-verification" 	content="ink6eHFnaYzCaXJfCf2n5vP08R6pLAd7z7CuB5Aq5sQ" />
+	</head>
+  <body>
+    <div class="header-black-background">
+      <img alt="sea quinn" src=./bioIncludes/pics/header-background.jpg class="responsive-img seaquinn"/>
+      <div class="header-text-wrapper">
+        <div class="header-text1">Sea Quinn</div>
+        <h1>Andrew Quinn</h1>
+        <h3>Software Architect. Woodworker. Groovy Individual.</h3>
+      </div>
+    </div>
+
+    <div class="special-global-wrapper">
+      <div class="special-wrapper">
+        <div class="special-section1">
+          <h1>What makes me so special?</h1>
+          <p>
+            I'm a graduate of Michigan State University with simultaneous
+            degrees in computer science, industrial psychology and minor in art
+            history. I began my career at Accenture, followed by creating dozens
+            of web-based applications and then helping to start what was for a
+            short time a billion-dollar startup. After living the super-fast
+            paced life of a consultant and software architect through the
+            booming of the Internet, I now live a quieter, but nonetheless
+            technically challenging, life running my software business, Sea
+            Quinn, from an 80 acres forest in Michigan's Upper Peninsula. Life
+            on the shores of Lake Superior is unmatched with vast beaches in the
+            summer and winters with over 200 inches of snow. The life of skiing
+            and snowshoeing in the winter and hiking and swimming in the summer
+            are shared with my wife, Amy, and our two dogs, Wondermutt the
+            Australian cattle dog and Maisy the Rottweiler.
+          </p>
+          <h4>Why Sea Quinn?</h4>
+          <p>
+            Sea Quinn was the name of my dad's boat. So many great memories that
+            are now memorialized with my company name.
+          </p>
+        </div>
+
+        <div class="special-section2">
+          <img
+            alt="Drew Quinn"
+            class="responsive-img"
+            src="./bioIncludes/pics/me.jpg"
+          />
+          <p>
+            "The unchanged passion of my life has been to take ideas and make
+            them reality. Whether they are technical innovations or things made
+            of wood, I've got the expertise to make it happen."
+          </p>
+        </div>
+      </div>
+    </div>
+
+	</body>
+</html>
+
+<script src="bioIncludes/App.js"></script>
