c25e9a097be0221f7d7494940d1c871ae5e77341
Drew Quinn <apquinn@gmail.com>
2022-12-06 14:14:42 -0500

    index.shtml

--- envelopes.sea-quinn.com/cmsphp/Finances_Nav.php
+++ envelopes.sea-quinn.com/cmsphp/Finances_Nav.php
@@ -5,11 +5,11 @@ $_SESSION[Const_sLoginName] = "quinn";
 
 
 print'<div class="row"><div class="col-md-8 mint_nav">';
-	print'<a class="allcaps" href="Finances.shtml?action=ImportData&phase=phase1&start=true">Import</a> &nbsp;|&nbsp; ';
-	print'<a class="allcaps" href="Finances.shtml?action=Trans&phase=phase1&start=true">Transactions</a> &nbsp;|&nbsp; ';
-	print'<a class="allcaps" href="Finances.shtml?action=Budget&phase=phase1&start=true">Budget</a>';
-	#print' &nbsp;|&nbsp; <a class="allcaps" href="Finances.shtml?action=Compare&phase=phase1&start=true">Compare Transactions</a>';
-	#print' &nbsp;|&nbsp; <a class="allcaps" href="Finances.shtml?action=TransGroups&phase=phase1&start=true">Group Transactions</a>';
+	print'<a class="allcaps" href="index.shtml?action=ImportData&phase=phase1&start=true">Import</a> &nbsp;|&nbsp; ';
+	print'<a class="allcaps" href="index.shtml?action=Trans&phase=phase1&start=true">Transactions</a> &nbsp;|&nbsp; ';
+	print'<a class="allcaps" href="index.shtml?action=Budget&phase=phase1&start=true">Budget</a>';
+	#print' &nbsp;|&nbsp; <a class="allcaps" href="index.shtml?action=Compare&phase=phase1&start=true">Compare Transactions</a>';
+	#print' &nbsp;|&nbsp; <a class="allcaps" href="index.shtml?action=TransGroups&phase=phase1&start=true">Group Transactions</a>';
 print'</div></div>';
 
 
